3aa9ec6690f6a5daf4d9170e2332c1d62f1a0d71
Anri Kigai <anrikigai@pm.me>
2021-04-04 21:30:58 +0300

    v0.20 CI.log added, linter suggestions applied

--- README.md
+++ README.md
@@ -11,10 +11,10 @@
 * После тестов из IDE выполняется commit / git push, что запускает github actions 
 * Запускается это на локальном runner (ARM на Raspberry Pi 4)
 * Устанавливается виртуальное окружение python3 и т.п.
-* Выполняются проверки линтером для директории 'src/'
+* Выполняются проверки линтером для директории `src/`
 * Запускается основная программа (в фоне), считывает показания температуры процессора (в будущем и внешних датчиков) и публикует на mqtt сервере
 * Запускается тестовая программа, подписывается на соответствующие топики mqtt, получает 2 сообщения в указанном диапазоне (из файла настроек)
-* Если все хорошо, завершает основную программу, создавая файл 'stop'
+* Если все хорошо, завершает основную программу и завершается сама
 * Копирует (scp) программу и настройки на Raspberry Pi Zero W (продуктивную) и запускает ее там, предварительно остановив "старую" копию
 
 ## master
@@ -29,24 +29,26 @@
 
 ### v0.12
 
-На целевой машине создан сервис '/etc/systemd/system/self_monitoring.service'
+На целевой машине создан сервис `/etc/systemd/system/self_monitoring.service`
 
-'''
-[Unit]
-Description=Self monitoring
-After=network.target
+    [Unit]
+    Description=Self monitoring
+    After=network.target
 
-[Service]
-Restart=always
-RestartSec=20
-User=pi
-ExecStart=/bin/bash -c "cd /home/pi/bin; ./self_monitoring.py" &
+    [Service]
+    Restart=always
+    RestartSec=20
+    User=pi
+    ExecStart=/bin/bash -c "cd /home/pi/bin; ./self_monitoring.py" &
 
-[Install]
-WantedBy=multi-user.target
-'''
+    [Install]
+    WantedBy=multi-user.target
 
-Также заданы необходимые переменные окружения по типу 'sudo systemctl set-environment MQTT_PORT=1883'  
+Также заданы необходимые переменные окружения по типу `sudo systemctl set-environment MQTT_PORT=1883`  
 После загрузки нового файла просто перезапускается сервис
 
+### v0.20
 
+Добавлен лог от предыдущего билда (`CI.log`)
+
+Внесены исправления в `self_monitoring.py` согласно рекомендациям линтера
--- src/self_monitoring.py
+++ src/self_monitoring.py
@@ -2,22 +2,24 @@
 VERSION = "0.10"
 
 import os
-import glob
+# import glob
 import time
 import signal
 import sys
 import configparser
 import paho.mqtt.client as mqtt_client
 
+
 ### -= Classes =-
 class mqtt(mqtt_client.Client):
     def __init__(self, client_id=""):
         super().__init__(client_id)
         self.device_id = client_id
+
     def publish(self, topic, payload=None, precision=0):
         if (isinstance(payload, float)):
-            #payload="{:.2f}".format(payload)
-            payload="{1:,.{0}f}".format(precision, payload)
+            # payload="{:.2f}".format(payload)
+            payload = "{1:,.{0}f}".format(precision, payload)
         super().publish("tele/"+self.device_id+"/"+topic, payload, qos=1, retain=True, properties=None)
 
 
@@ -40,18 +42,18 @@ def init():
         sys.exit()
     # print(MQTT_SERVER, MQTT_PORT, MQTT_USER, MQTT_PASS)
 
-    config_file=os.path.basename(__file__)[:os.path.basename(__file__).rindex(".")]+".ini"
+    config_file = os.path.basename(__file__)[:os.path.basename(__file__).rindex(".")]+".ini"
     global config
     config = configparser.ConfigParser()
     config.read(config_file)
 
     global DELAY
-    DELAY=int(config["MAIN"]["Delay"])
+    DELAY = int(config["MAIN"]["Delay"])
     global client
-    DEVICE_ID=config["MAIN"]["DeviceID"]
+    DEVICE_ID = config["MAIN"]["DeviceID"]
     client = mqtt(DEVICE_ID)
     client.username_pw_set(MQTT_USER, MQTT_PASS)
-    client.connect(MQTT_SERVER)
+    client.connect(host=MQTT_SERVER, port=MQTT_PORT)
     #client.publish("tele/"+device_id+"/Connected", time.strftime("%H:%M:%S %d/%m/%Y", time.localtime()))
     client.publish("Connected", time.strftime("%d/%m/%Y %H:%M:%S", time.localtime()))
     client.loop_start()
@@ -59,7 +61,7 @@ def init():
 
 ### -= Program starts =-
 def main():
-    sec_start=time.time()
+    sec_start = time.time()
     while True:
         try:
             tFile = open('/sys/class/thermal/thermal_zone0/temp')
@@ -69,11 +71,12 @@ def main():
             client.publish("LastUpdate", time.strftime("%d/%m/%Y %H:%M:%S", time.localtime()))
             time.sleep(DELAY)
         except KeyboardInterrupt:
-            print (" : Ctrl+C pressed, bye.", end="\n\n")
+            print(" : Ctrl+C pressed, bye.", end="\n\n")
             sys.exit()
     sec_end = time.time()
-    sec=sec_end-sec_start
+    sec = sec_end-sec_start
     print(sec)
+
 
 ###
 init()
